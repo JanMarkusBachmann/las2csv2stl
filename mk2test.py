9789d4b945195778bf6243ecdfe890569a0aec3e
JanMarkusBachmann <bachmannjanmarkus@gmail.com>
2024-02-20 18:03:04 +0200

    semitootab koik

--- mk2test.py
+++ mk2test.py
@@ -1,5 +1,6 @@
 import math
 import tkinter.filedialog
+import datetime
 
 
 def arvutakaal(kaugusx, kaugusy):
@@ -11,46 +12,81 @@ def arvutakaal(kaugusx, kaugusy):
         kaal = 1 / ((10 * pikus) ** 2)
         return kaal
 
+def leiaexpform(arv):
+    if arv != 0:
+        g = (math.log10(abs(arv)) // 1)
+        e = arv / (10 ** g)
+        if g < 0:
+            mark = '-'
+        else:
+            mark = '+'
+        if abs(g) < 10:
+            exp = f'e{mark}00{round(abs(g))}'
+        else:
+            exp = f'e{mark}0{round(abs(g))}'
+        return f'{round(e, 6)}{exp}'
+    else:
+        return '0.000000e+000'
+
 class Triangle:
     vektorbank = {}  # globaalne bank arvutatud vektorite jaoks formaadis - [X, Y]: [a ->b]
-    def __init__(self, a, b, c):
+    def __init__(self, a, b, c, type='up'):
         self.a = a  # punktid
         self.b = b
         self.c = c
-        self.ab = self.leiavektor()
+        self.type = type
+        self.ab = self.leiavektor(a, b)
+        self.ac = self.leiavektor(a, c)
+        self.bc = self.leiavektor(b, c)
+        self.n = self.leianormaal(self.ab, self.ac, self.type)
+
+    def __str__(self):
+        return f'Kolmnurk {self.a.x}, {self.a.y}, {self.a.synZ} - XYZ punkt A'
 
     def leiavektor(self, a, b):
-        if [a, b] in Triangle.vektorbank.keys():
-            return Triangle.vektorbank[[a, b]]
+        if (a, b) in Triangle.vektorbank.keys():
+            return Triangle.vektorbank[(a, b)]
         else:
             vx = b.x - a.x
             vy = b.y - a.y
             vz = b.synZ - a.synZ
-            v = [vx, vy, vz]
-            Triangle.vektorbank.update({[a, b]: v})
+            v = (vx, vy, vz)
+            Triangle.vektorbank.update({(a, b): v})
             return v
 
     def leianormaal(self, a, b, tyyp='up'):
-        xm = a.x
-        xn = b.x
-        ym = a.y
-        yn = b.y
-        zm = a.synZ
-        zn = b.synZ
+        xm = a[0]
+        xn = b[0]
+        ym = a[1]
+        yn = b[1]
+        zm = a[2]
+        zn = b[2]
         # avrutame determinantidega vektor korrutise
         x = ym * zn - yn * zm
         y = -(xm * zn - xn * zm)
         z = xm * yn - ym * xn
+        kordaja = math.sqrt(x**2 + y**2 + z**2)
+        x = x/kordaja
+        y = y/kordaja
+        z = z/kordaja
         if tyyp == 'up':
             if z < 0:
-                return [x, y, z]
+                return (x, y, z)
             else:
-                return [-x, -y, -z]
+                return (-x, -y, -z)
         elif tyyp == 'down':
             if z > 0:
-                return [x, y, z]
+                return (x, y, z)
             else:
-                return [-x, -y, -z]
+                return (-x, -y, -z)
+
+    def stlprintout(self):
+        adot = f'vertex {leiaexpform(self.a.x)} {leiaexpform(self.a.y)} {leiaexpform(self.a.synZ)}'
+        bdot = f'vertex {leiaexpform(self.b.x)} {leiaexpform(self.b.y)} {leiaexpform(self.b.synZ)}'
+        cdot = f'vertex {leiaexpform(self.c.x)} {leiaexpform(self.c.y)} {leiaexpform(self.c.synZ)}'
+        norm = f'facet normal {leiaexpform(self.n[0])} {leiaexpform(self.n[1])} {leiaexpform(self.n[2])}'
+        return f'\t{norm}\n\t\touter loop\n\t\t\t{adot}\n\t\t\t{bdot}\n\t\t\t{cdot}\n\t\tendloop\n\tendfacet'
+
 
 
 class Syndot:
@@ -119,6 +155,8 @@ class Mesh:
         self.createsyndotdic()
         self.sorteeridata()
         self.arvutaZsyndotidele()
+        self.kolmnurgad = []
+        self.genkolmnurad()
     def createsyndotdic(self):  # tekitab ilma sisuta valmis synttetiliste andmepunktide ruudustiku
         i = 0  #x
         j = 0  #y
@@ -130,32 +168,61 @@ class Mesh:
                 j += 1
             self.syndots.update({i: dempdic})
             i += 1
-        print('Synmesh created')
+        print(f'Synmesh created {datetime.datetime.now()}')
     def sorteeridata(self):  # tekitab realdot objektid ning koheselt paigutrab need syndot naabrite dictioneridesse
         for punkt in self.data:
             pktRN = Realdot(punkt[0], punkt[1], punkt[2], self.sqrsize)
             pktRN.paigutasyndot(self)
-        print('Punktid on sorteeritud syndotidesse!')
+        print(f'Punktid on sorteeritud syndotidesse! {datetime.datetime.now()}')
 
     def arvutaZsyndotidele(self):
         for x in self.syndots.keys():
             for y in self.syndots.keys():
                 self.syndots[x][y].arvutaZ()
-        print('keksmine z arvutud')
+        print(f'keksmine z arvutud {datetime.datetime.now()}')
+
+    def genkolmnurad(self):
+        i = 0 #x
+        while i < self.sizex:
+            j = 0  #y
+            while j < self.sizey:
+                a = self.syndots[i][j]
+                b = self.syndots[i + 1][j]
+                c = self.syndots[i + 1][j + 1]
+                d = self.syndots[i][j + 1]
+                tri1 = Triangle(a, b, c)
+                tri2 = Triangle(a, d, c)
+                self.kolmnurgad.append(tri1)
+                self.kolmnurgad.append(tri2)
+                j += 1
+            i += 1
+        print(f'kolmnurgad genereeritud {datetime.datetime.now()}')
+
+    def exportmesh(self):
+        txt = []
+        nimi = input('Vali oma objektile nimi')
+        txt.append(f'solid {nimi}')
+        for tri in self.kolmnurgad:
+            txt.append(tri.stlprintout())
+        txt.append('endsolid')
+        r = '\n'.join(txt)
+        return r, nimi
     def __str__(self):
         return f'mesh {self.sizex}, {self.sizey} - XY, andmepunkte on {len(self.data)}'
 
 
 def csv2list(path=None):  # loeb csv faili ja tagastab listi punktide kordinaatidega
+    pathni = path
     if path == None:
         pathnimi = str(tkinter.filedialog.askopenfile())
-        pathn = pathnimi.split("'")[1]
+        pathni = pathnimi.split("'")[1]
+    print(datetime.datetime.now())
     data = []
     minx = float(0)
     maxx = float(0)
     miny = float(0)
     maxy = float(0)
-    file = open(pathn, encoding='UTF-8')
+    file = open(pathni, encoding='UTF-8')
     for rida in file:
         if rida != 'X,Y,Z\n':
             string = rida.strip('\n')
@@ -177,9 +244,16 @@ def csv2list(path=None):  # loeb csv faili ja tagastab listi punktide kordinaati
         datanorm.append(RN)
     return datanorm, 0.0, maxx-minx, 0.0, maxy-miny
 
+
 data, minx, maxx, miny, maxy = csv2list()
 datasize = len(data)
 sqrsize = 1
 sizex = math.ceil(maxx/sqrsize)
 sizey = math.ceil(maxy/sqrsize)
+print(datetime.datetime.now())
 m1 = Mesh(data, sizex, sizey, sqrsize)
+mesh, nimi = m1.exportmesh()
+f = open(f'{nimi}.stl', 'w', encoding='UTF-8')
+print(datetime.datetime.now())
+f.write(mesh)
+print('valmis')
